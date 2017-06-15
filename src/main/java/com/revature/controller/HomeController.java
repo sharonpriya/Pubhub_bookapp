16ec076c4e5d0bce05d6a887df1ba4a4809bd0e8
sharonpriya <sharonpriya@bsauniv.ac.in>
2017-06-15 11:18:34 +0530

    Create HomeController.java

--- src/main/java/com/revature/controller/HomeController.java
+++ src/main/java/com/revature/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 	@GetMapping("/")
-	public String index(){
+	public String front(){
 		System.out.println("Index");
 		return "index";
 	}
